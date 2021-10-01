25a43b810e551d7c11a0986014c0f65276a77356
v.totskiy <totskiy00702gmail.com>
2021-10-01 14:34:24 +0300

    added new app, added all models

--- events/models.py
+++ events/models.py
@@ -2,6 +2,6 @@ from django.db import models
 
 # Create your models here.
 
-class Events(models.Model):
+class Event(models.Model):
     event_image = models.ImageField(upload_to='event_images/')
     event_text = models.CharField(max_length=300)
--- myfirstblog/settings.py
+++ myfirstblog/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'blog.apps.BlogConfig',
     'events.apps.EventsConfig',
     'django.contrib.admin',
     'django.contrib.auth',
@@ -76,8 +77,12 @@ WSGI_APPLICATION = 'myfirstblog.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'myfirstblogdb',
+        'USER': 'postgres',
+        'PASSWORD': 'admin',
+        'HOST': '127.0.0.1',
+        'PORT': '5432'
     }
 }
 
@@ -128,4 +133,4 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 import os
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
-MEDIA_URL = '/media/'
+MEDIA_URL = '/wowmedia/'
