20167311d8d1c1b2d9be59386aa101a4855844e3
v.totskiy <totskiy00702gmail.com>
2021-10-02 23:10:44 +0300

    added new app, added all models apps, functionality finish!

--- myfirstblog/settings.py
+++ myfirstblog/settings.py
@@ -118,11 +118,16 @@ USE_I18N = True
 USE_L10N = True
 
 USE_TZ = True
+import os
 
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'myfirstblog/static/')
+]
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 STATIC_URL = '/static/'
 
 # Default primary key field type
@@ -130,7 +135,7 @@ STATIC_URL = '/static/'
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
-import os
+
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 MEDIA_URL = '/wowmedia/'
